74b095dc369e56171dd4ae6667d15fb859e85dbf
miguelkashir <miguelkashir@gmail.com>
2016-12-09 12:54:09 +0100

    install gem materialize, add seeds :gem::art:

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-3.1.1.min.js
-//= require materialize.min.js
+//= require materialize-sprockets
 //= require macromeal.js
-// require turbolinks
 //= require_tree .
