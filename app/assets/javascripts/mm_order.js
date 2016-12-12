71b6ecc56b17ce7509d07b0388b1017ae1267a2b
miguelkashir <miguelkashir@gmail.com>
2016-12-12 15:37:36 +0100

    order wip 6 (add-substract not working 100%) :construction:

--- app/assets/javascripts/mm_order.js
+++ app/assets/javascripts/mm_order.js
@@ -3,7 +3,6 @@ var mealsCounter = 0;
 var productsCounter = 0;
 var currentMeal;
 
-//init loads
 function initialize() {
   $('.js-nav-links li').removeClass('active');
   $('.js-nav-link-order').addClass('active');
@@ -11,40 +10,28 @@ function initialize() {
   $('#modal-products').modal();
 }
 
-// js-add-meal callback, adds a meal block
 function addMeal() {
   var idMeal = mealsCounter++; //unique id for meal
-
   var meal = '<li class="collection-item" id="meal'+ idMeal +'">';
   meal += '<table class="js-meal highlight centered"><thead><tr>';
   meal += '<th data-field="name">Producto</th>';
-  meal += '<th data-field="calories">Calorías</th>';
-  meal += '<th data-field="protein">Proteínas</th>';
-  meal += '<th data-field="fat">Grasas</th>';
-  meal += '<th data-field="carbs">Carbohidratos</th>';
   meal += '<th data-field="price">Cantidad</th>';
   meal += '<th data-field="edit">Modificar</th>';
   meal += '<th data-field="delete">Eliminar</th>';
   meal += '<th data-field="price">Precio</th>';
   meal += '</tr></thead><tbody></tbody></table><br>';
-
-  //add product to meal
   meal += '<a class="js-add-products waves-effect waves-light btn lime" id="js-add-products-meal'+ idMeal +'">';
   meal += '<i class="material-icons left">add</i>Añadir Producto</a> ';
-
-  //delete meal
   meal += '<a class="js-del-meal waves-effect waves-light btn red">';
   meal += '<i class="material-icons">delete</i></a></li>';
 
   $('.js-collection').append(meal);
-  $('.js-add-products').click(loadProducts.bind(event));
+  $('.js-add-products').click(getProducts.bind(event));
   $('.js-del-meal').click(deleteMeal);
 }
 
-//js-add-products callback, AJAX query loading products
-function loadProducts(event) {
+function getProducts(event) {
   idMeal = event.currentTarget.parentElement.id;
-
   $.ajax({
     type: 'GET',
     url: '/api/products',
@@ -76,58 +63,72 @@ function showProducts(idMeal, response) {
   }
   $('.js-products').empty();
   $('.js-products').append(products);
-  $('.js-add-product').click(addProduct);
+  $('.js-add-product').click(getProduct);
 }
 
-//js-add-product callback, adds product to meal
-function addProduct(event) {
+function getProduct(event) {
   var idProduct = event.currentTarget.id;
 
   $.ajax({
     type: 'GET',
     url: '/api/products/' + idProduct,
-    success: function(response) {
-      var idProduct = 'product'+ productsCounter++; //unique id for product
-
-      var product = '<tr id="'+ idProduct +'">';
-      product += '<td>'+ response.name +'</td>';
-      product += '<td>'+ response.calories +'</td>';
-      product += '<td>'+ response.protein +'</td>';
-      product += '<td>'+ response.fat +'</td>';
-      product += '<td>'+ response.carbs +'</td>';
-
-      if (response.category !== 'bebida') {
-        product += '<td>100 gr</td>';
-      }
-      else {
-        product += '<td>1</td>';
-      }
-
-      product += '<td>';
-      product += '<a class="waves-effect waves-light btn"><i class="material-icons">add</i></a>';
-      product += ' '; //buttons spacer
-      product += '<a class="waves-effect waves-light btn grey disabled"><i class="material-icons">remove</i></a>';
-      product += '</td>';
-      product += '<td>';
-      product += '<a class="js-del-product waves-effect waves-light btn red"><i class="material-icons">delete</i></a>';
-      $('.js-del-product').click(deleteProduct);
-      product += '</td>';
-      product += '<td>'+ response.price +' €</td>';
-      product += '</tr>';
-
-      msg('Añadido: '+ response.name, 1000);
-      $('#'+ currentMeal +' table tbody').append(product);
-      $('.js-del-product').click(deleteProduct);
-    }
+    success: addProduct
   });
 }
 
+function addProduct(response) {
+  var idProduct = 'product'+ productsCounter++; //unique id for product
+
+  var product = '<tr id="'+ idProduct +'">';
+  product += '<td>'+ response.name +'</td>';
+  product += '<td class="amount">100 gr</td>';
+  product += '<td><a class="js-add-amount waves-effect waves-light btn">';
+  product += '<i class="material-icons">add</i></a>';
+  product += '<a class="js-substract-amount waves-effect waves-light btn grey disabled">';
+  product += '<i class="material-icons">remove</i></a></td>';
+  product += '<td><a class="js-del-product waves-effect waves-light btn red">';
+  product += '<i class="material-icons">delete</i></a></td>';
+  product += '<td>'+ response.price +' €</td>';
+  product += '</tr>';
+
+  $('#'+ currentMeal +' table tbody').append(product);
+  msg('Añadido: '+ response.name, 1000);
+  $('.js-add-amount').click(addAmount);
+  $('.js-substract-amount').click(substractAmount);
+  $('.js-del-product').click(deleteProduct);
+}
+
 function deleteMeal(event) {
-  var meal_id = event.currentTarget.parentElement.id;
-  $('#'+ meal_id +'').remove();
+  var idMeal = event.currentTarget.parentElement.id;
+  $('#'+ idMeal).remove();
 }
 
 function deleteProduct(event) {
-  var product_id = event.currentTarget.parentElement.parentElement.id;
-  $('#'+ product_id +'').remove();
+  var idProduct = event.currentTarget.parentElement.parentElement.id;
+  console.log(idProduct);
+  $('#'+ idProduct).remove();
+}
+
+function addAmount(event) {
+  var idProduct = event.currentTarget.parentElement.parentElement.id;
+  console.log(idProduct);
+  var currentAmount = parseInt(($('#'+ idProduct +' .amount').text()).split(' ')[0]);
+  currentAmount += 100;
+  $('#'+ idProduct +' .amount').text(currentAmount +' gr');
+  if(currentAmount > 100) {
+    $('#'+ idProduct +' .js-substract-amount').removeClass('disabled');
+    $('#'+ idProduct +' .js-substract-amount').removeClass('grey');
+    $('#'+ idProduct +' .js-substract-amount').addClass('red accent-1');
+  }
+}
+
+function substractAmount(event) {
+  var idProduct = event.currentTarget.parentElement.parentElement.id;
+  var currentAmount = parseInt($('#'+ idProduct +' .amount').text().split(' ')[0]);
+  currentAmount -= 100;
+  $('#'+ idProduct +' .amount').text(currentAmount +' gr');
+  if(currentAmount === 100) {
+    $('#'+ idProduct +' .js-substract-amount').addClass('disabled');
+    $('#'+ idProduct +' .js-substract-amount').addClass('grey');
+  }
 }
